6873bbb22f632037d387520c40e7834250d11895
Eric K Wu <ekuan.wu@gmail.com>
2019-12-23 15:09:45 -0800

    Contact theming

--- wp-content/themes/chenco/page-contact.php
+++ wp-content/themes/chenco/page-contact.php
@@ -3,7 +3,7 @@
 /* Template Name: Contact Page */
 
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
+  exit; // Exit if accessed directly.
 }
 
 get_header(); ?>
@@ -11,12 +11,12 @@ get_header(); ?>
 <div id="primary" <?php generate_do_element_classes('content'); ?>>
   <main id="main" <?php generate_do_element_classes('main'); ?>>
     <?php
-        /**
-         * generate_before_main_content hook.
-         *
-         * @since 0.1
-         */
-        do_action('generate_before_main_content'); ?>
+    /**
+     * generate_before_main_content hook.
+     *
+     * @since 0.1
+     */
+    do_action('generate_before_main_content'); ?>
 
     <?php while (have_posts()) : the_post(); ?>
 
@@ -34,38 +34,45 @@ get_header(); ?>
         <div class="contact-people-item">
           <img class="contact-img" src="<?php the_sub_field('image'); ?>" />
           <div class="contact-people-info">
-            <h3><?php the_sub_field('region'); ?></h3>
+            <strong><?php the_sub_field('region'); ?></strong>
             <p><?php the_sub_field('name'); ?></p>
-            <p>+<?php the_sub_field('phone'); ?></p>
-            <p>#<?php the_sub_field('extension'); ?></p>
+            <?php if (get_sub_field('phone')) : ?>
+            <p>+<?php the_sub_field('phone'); ?> #<?php the_sub_field('extension'); ?></p>
+            <?php endif; ?>
+
           </div>
         </div>
         <?php endwhile; ?>
       </div>
 
+
       <?php while (have_rows('contact_locations')) : the_row(); ?>
-      <h2 class="heading"><?php the_sub_field('region'); ?></h2>
-      <div class="contact-location-container">
-        <?php while (have_rows('region_info')) : the_row(); ?>
-        <div class="contact-location-item">
-          <h3><?php the_sub_field('location'); ?></h3>
-          <p class="contact-address"><?php the_sub_field('address'); ?></p>
-          <p>+<?php the_sub_field('phone'); ?></p>
-          <p><?php the_sub_field('website'); ?></p>
+      <div class="contact__location">
+        <h2 class="heading"><?php the_sub_field('region'); ?></h2>
+        <div class="contact-location-container">
+          <?php while (have_rows('region_info')) : the_row(); ?>
+          <div class="contact-location-item">
+            <strong><?php the_sub_field('location'); ?></strong>
+            <p class="contact-address"><?php the_sub_field('address'); ?></p>
+            <?php if (get_sub_field('phone')) : ?>
+            <p>+<?php the_sub_field('phone'); ?></p>
+            <?php endif; ?>
+            <p><?php the_sub_field('website'); ?></p>
+          </div>
+          <?php endwhile; ?>
         </div>
-        <?php endwhile; ?>
       </div>
       <?php endwhile; ?>
     </div>
 
     <?php
-        /**
-         * generate_after_main_content hook.
-         *
-         * @since 0.1
-         */
-        do_action('generate_after_main_content');
-        ?>
+    /**
+     * generate_after_main_content hook.
+     *
+     * @since 0.1
+     */
+    do_action('generate_after_main_content');
+    ?>
   </main><!-- #main -->
 </div><!-- #primary -->
 
