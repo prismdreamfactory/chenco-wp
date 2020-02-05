384fa6117bd34355838ae69b0c94634b26270e87
Eric K Wu <ekuan.wu@gmail.com>
2020-02-05 11:44:14 -0800

    News layout

--- wp-content/themes/chenco/content.php
+++ wp-content/themes/chenco/content.php
@@ -4,42 +4,48 @@
  * The template for displaying posts within the loop.
  */
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
+  exit; // Exit if accessed directly.
 }
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata('article'); ?>>
-  <div class="inside-article">
-    <?php
+<div class="container">
+
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php generate_do_microdata('article'); ?>>
+    <div class="inside-article inside-article--news">
+      <?php
+      /**
+       * generate_before_content hook.
+       *
+       * @since 0.1
+       *
+       * @hooked generate_featured_page_header_inside_single - 10
+       */
+      do_action('generate_before_content');
+      ?>
+
+      <header class="entry-header">
+
+        <?php
         /**
-         * generate_before_content hook.
+         * generate_before_entry_title hook.
          *
          * @since 0.1
-         *
-         * @hooked generate_featured_page_header_inside_single - 10
          */
-        do_action('generate_before_content');
+        do_action('generate_before_entry_title');
         ?>
 
-    <header class="entry-header">
-      <?php
-            /**
-             * generate_before_entry_title hook.
-             *
-             * @since 0.1
-             */
-            do_action('generate_before_entry_title');
-            ?>
+        <h4 class="heading"><?php the_date(); ?></h4>
 
-      <h4 class="heading"><?php the_date(); ?></h4>
+        <div class="entry">
+          <?php
 
-      <?php
+          the_title(sprintf('<h2 class="entry-title post__title" itemprop="headline"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
+          ?>
 
-            the_title(sprintf('<h2 class="entry-title post__title" itemprop="headline"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>');
-            ?>
+          <!-- <a href="<?php the_permalink(); ?>">Read More</a> -->
+        </div>
 
-      <a href="<?php the_permalink(); ?>" class='btn alt'>Read More</a>
 
-      <!-- /*
+        <!-- /*
              * generate_after_entry_title hook.
              *
              * @since 0.1
@@ -47,54 +53,56 @@ if (!defined('ABSPATH')) {
              * @hooked generate_post_meta - 10
              */
             // do_action('generate_after_entry_title'); -->
-    </header><!-- .entry-header -->
+      </header><!-- .entry-header -->
 
-    <?php
-        /**
-         * generate_after_entry_header hook.
-         *
-         * @since 0.1
-         *
-         * @hooked generate_post_image - 10
-         */
-        do_action('generate_after_entry_header');
+      <?php
+      /**
+       * generate_after_entry_header hook.
+       *
+       * @since 0.1
+       *
+       * @hooked generate_post_image - 10
+       */
+      do_action('generate_after_entry_header');
 
-        if (generate_show_excerpt()) : ?>
+      if (generate_show_excerpt()) : ?>
 
-    <div class="entry-summary" itemprop="text">
-      <!-- <?php the_excerpt(); ?> -->
-    </div><!-- .entry-summary -->
+      <?php /* <div class="entry-summary" itemprop="text">
+     <?php the_excerpt(); ?>
+    </div><!-- .entry-summary --> */ ?>
 
     <?php else : ?>
 
     <div class="entry-content" itemprop="text">
       <?php
-                the_content();
+          the_content();
 
-                wp_link_pages(array(
-                    'before' => '<div class="page-links">' . __('Pages:', 'generatepress'),
-                    'after' => '</div>',
-                ));
-                ?>
+          wp_link_pages(array(
+            'before' => '<div class="page-links">' . __('Pages:', 'generatepress'),
+            'after' => '</div>',
+          ));
+          ?>
     </div><!-- .entry-content -->
 
     <?php endif;
 
-        // /**
-        //  * generate_after_entry_content hook.
-        //  *
-        //  * @since 0.1
-        //  *
-        //  * @hooked generate_footer_meta - 10
-        //  */
-        // do_action( 'generate_after_entry_content' );
+      // /**
+      //  * generate_after_entry_content hook.
+      //  *
+      //  * @since 0.1
+      //  *
+      //  * @hooked generate_footer_meta - 10
+      //  */
+      // do_action( 'generate_after_entry_content' );
 
-        // /**
-        //  * generate_after_content hook.
-        //  *
-        //  * @since 0.1
-        //  */
-        // do_action( 'generate_after_content' );
-        ?>
-  </div><!-- .inside-article -->
+      // /**
+      //  * generate_after_content hook.
+      //  *
+      //  * @since 0.1
+      //  */
+      // do_action( 'generate_after_content' );
+      ?>
+</div><!-- .inside-article -->
 </article><!-- #post-## -->
+
+</div>
