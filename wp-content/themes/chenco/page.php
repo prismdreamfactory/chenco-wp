f110b577a9cd969c5d8081abbafe380bbcd5da81
Eric K Wu <ekuan.wu@gmail.com>
2020-05-04 14:31:34 -0700

    Restore last

--- wp-content/themes/chenco/page.php
+++ wp-content/themes/chenco/page.php
@@ -18,48 +18,54 @@ if (!defined('ABSPATH')) {
 get_header(); ?>
 
 
+
 <div id="primary" <?php generate_do_element_classes('content'); ?>>
   <main id="main" <?php generate_do_element_classes('main'); ?>>
-    <?php
-    /**
-     * generate_before_main_content hook.
-     *
-     * @since 0.1
-     */
-    do_action('generate_before_main_content');
 
-    while (have_posts()) : the_post();
+    <div class="container">
+      <?php
+      /**
+       * generate_before_main_content hook.
+       *
+       * @since 0.1
+       */
+      do_action('generate_before_main_content');
 
-      get_template_part('content', 'page');
+      while (have_posts()) : the_post();
 
-      // If comments are open or we have at least one comment, load up the comment template.
-      if (comments_open() || '0' != get_comments_number()) :
-        /**
-         * generate_before_comments_container hook.
-         *
-         * @since 2.1
-         */
-        do_action('generate_before_comments_container');
-    ?>
+        get_template_part('content', 'page');
 
-    <div class="comments-area">
-      <?php comments_template(); ?>
+        // If comments are open or we have at least one comment, load up the comment template.
+        if (comments_open() || '0' != get_comments_number()) :
+          /**
+           * generate_before_comments_container hook.
+           *
+           * @since 2.1
+           */
+          do_action('generate_before_comments_container');
+      ?>
+
+      <div class="comments-area">
+        <?php comments_template(); ?>
+      </div>
+
+      <?php
+        endif;
+
+      endwhile;
+
+      /**
+       * generate_after_main_content hook.
+       *
+       * @since 0.1
+       */
+      do_action('generate_after_main_content');
+      ?>
+
     </div>
-
-    <?php
-      endif;
-
-    endwhile;
-
-    /**
-     * generate_after_main_content hook.
-     *
-     * @since 0.1
-     */
-    do_action('generate_after_main_content');
-    ?>
   </main><!-- #main -->
 </div><!-- #primary -->
+
 
 
 <?php
